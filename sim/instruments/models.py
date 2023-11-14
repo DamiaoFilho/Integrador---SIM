365b7e557dcc9bf1c080a2bb5ba1126cbf405bd6
DamiaoFilho <damiaofilho0123@gmail.com>
2023-11-13 21:34:56 -0300

    ajuste de estilo

--- sim/instruments/models.py
+++ sim/instruments/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Instrument(models.Model):
+    image = models.ImageField(upload_to="instruments/images")
+    name = models.CharField(max_length=150)
+    modelo = models.CharField(max_length=150)
+    serial_number = models.IntegerField()
+    color = models.CharField()
+    condition = models.TextField()
+    status = models.BooleanField()
+    
